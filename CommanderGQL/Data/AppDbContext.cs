0e9cd9098d55ad7d2917eee55f519e71cfef4726
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-08-20 21:25:53 +0300

    Add db connection string, create db context and Platform model

---
+++ CommanderGQL/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CommanderGQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderGQL.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Platform> Platforms { get; set; }
+}
