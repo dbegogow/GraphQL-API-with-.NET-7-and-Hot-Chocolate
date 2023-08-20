9b6b8cf59919842564f0d328066def635450618b
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-08-20 21:32:20 +0300

    Create extension method for adding database

---
+++ CommanderGQL/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CommanderGQL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderGQL.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        => services
+            .AddDbContext<AppDbContext>(options => options
+                .UseSqlServer(configuration.GetConnectionString("CommandConStr")));
+}
--- CommanderGQL/Program.cs
+++ CommanderGQL/Program.cs
@@ -1,4 +1,9 @@
+using CommanderGQL.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDatabase(builder.Configuration);
 
 var app = builder.Build();
 
