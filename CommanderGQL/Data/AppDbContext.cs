490843b4b884175d4722517ce50379882ec0b0b9
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-09-01 23:41:00 +0300

    Add AddCommandToDB migration

--- CommanderGQL/Data/AppDbContext.cs
+++ CommanderGQL/Data/AppDbContext.cs
@@ -13,4 +13,21 @@ public class AppDbContext : DbContext
     public DbSet<Platform> Platforms { get; set; }
 
     public DbSet<Command> Commands { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Platform>()
+            .HasMany(p => p.Commands)
+            .WithOne(c => c.Platform!)
+            .HasForeignKey(c => c.PlatformId);
+
+        modelBuilder
+            .Entity<Command>()
+            .HasOne(c => c.Platform)
+            .WithMany(p => p.Commands)
+            .HasForeignKey(c => c.PlatformId);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
